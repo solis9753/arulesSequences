0f32f2cbd7af35052fab2131256e71b44db27e9a
Solon Ioannou <sioann88@gmail.com>
2022-05-26 15:45:35 +0200

    - changing package name to arulesSequencesForkedCustom

---
+++ R/arulesSequencesForkedCustom.R
@@ -0,0 +1,8 @@
+#' arulesSequencesForkedCustom: Perform frequent pattern mining and save the results in binary. 
+#' 
+#' @docType package
+#' @name arulesSequencesForkedCustom
+#' @useDynLib arulesSequencesForkedCustom, .registration=TRUE
+NULL
+
+#> NULL
